f9d16c8ade85b8d2db1f807fa2f3ca04f14fce47
dan-thuy lam <lamd@igh-0120.unine.ch>
2018-09-11 18:40:10 +0200

    remove variomodels library

--- makeTruncGaussian.py
+++ makeTruncGaussian.py
@@ -3,7 +3,6 @@
 import numpy as np
 from matplotlib import pyplot as plt
 import tpg
-import variomodels
 
 # Simulate one continuous gaussian realizations
 gaussian = tpg.genGaussianSim_2D(30, 30, 1, 1, 'exponential', 10)
@@ -20,3 +19,5 @@ faciesMap = tpg.truncGaussian2facies(gaussian, 0.3, 0.2, 0.5)
 plt.close()
 tpg.discrete_imshow(faciesMap)
 plt.savefig('faciesMap0.png', dpi=300)
+
+
