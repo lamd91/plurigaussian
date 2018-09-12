b9c41591bfcef71f6fd1768a299e8b90bfca1481
dan-thuy lam <lamd@igh-0120.unine.ch>
2018-09-12 23:25:44 +0200

    first attempt at plurigaussian simulation using 2 gaussian realizations and a lithotype rule allowing the simulation of up to facies (2 threshold lines were used)

--- makeTruncBiGaussian.py
+++ makeTruncBiGaussian.py
@@ -8,20 +8,18 @@ from matplotlib import pyplot as plt
 gaussian1 = tpg.genGaussianSim_2D(30, 30, 1, 1, 'exponential', 10)
 gaussian2 = tpg.genGaussianSim_2D(30, 30, 1, 1, 'gaussian', 5)
 
-## Display each generated gaussian realizations
-#plt.imshow(gaussian1, cmap='rainbow', aspect='auto')
-#plt.colorbar()
-#plt.savefig('gaussian1.png', dpi=300)
-#plt.close()
-#plt.imshow(gaussian2, cmap='rainbow', aspect='auto')
-#plt.colorbar()
-#plt.savefig('gaussian2.png', dpi=300)
-#plt.close()
+# Display each generated gaussian realizations
+plt.imshow(gaussian1, cmap='rainbow', aspect='auto')
+plt.colorbar()
+plt.savefig('gaussian1.png', dpi=300)
+plt.close()
+plt.imshow(gaussian2, cmap='rainbow', aspect='auto')
+plt.colorbar()
+plt.savefig('gaussian2.png', dpi=300)
+plt.close()
 
 # Truncate according to given rock type rule 
 thresholdLines = tpg.truncLines() # generate two random threshold lines 
-#A = tpg.thresholdLineEq(thresoldLines.dist2origin[0], thresoldLines.angles[0], gaussian1)
-#print(A)
 faciesMap = tpg.truncBiGaussian2facies(gaussian1, gaussian2, thresholdLines)
 
 # Display the derived facies map with a discrete colorbar 
