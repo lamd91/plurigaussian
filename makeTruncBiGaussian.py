a379f52fdb16572edce66bcf25c15b67cd1ad1d5
dan-thuy lam <lamd@igh-0120.unine.ch>
2018-09-19 12:17:28 +0200

    --allow-empty-message

--- makeTruncBiGaussian.py
+++ makeTruncBiGaussian.py
@@ -1,25 +1,18 @@
 #!/usr/bin/python3
 
 import tpg
-import tpg_aniso
 import numpy as np
 from matplotlib import pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 # Simulate two continuous gaussian realizations using different variogram models
-#gaussian1 = tpg.genGaussianSim_2D(30, 30, 1, 1, 'gaussian', 10)
-#gaussian2 = tpg.genGaussianSim_2D(30, 30, 1, 1, 'gaussian', 10)
-#gaussian1 = tpg.genGaussianSim_2D(60, 60, 0.5, 0.5, 'spherical', 10)
-#gaussian2 = tpg.genGaussianSim_2D(60, 60, 0.5, 0.5, 'spherical', 10)
-#gaussian1 = tpg.genGaussianSim_2D(60, 60, 0.5, 0.5, 'exponential', 10)
-#gaussian2 = tpg.genGaussianSim_2D(60, 60, 0.5, 0.5, 'exponential', 10)
-gaussian1 = tpg_aniso.genGaussianSim_2D_aniso(120, 120, 0.5, 0.5, 'exponential', 10, 0.5, 45)
-gaussian2 = tpg.genGaussianSim_2D(30, 30, 1, 1, 'exponential', 10)
+gaussian1 = tpg.genGaussianSim_2D_aniso(120, 120, 0.5, 0.5, 'exponential', 10, 0.6, 45)
+gaussian2 = tpg.genGaussianSim_2D_aniso(120, 120, 0.5, 0.5, 'exponential', 20, 0.25, 0)
 
 # Display each generated gaussian realizations
 plt.close()
 fig, ax = plt.subplots()
-im = plt.imshow(gaussian1, cmap='rainbow')
+im = plt.imshow(gaussian1, origin='lower', cmap='rainbow')
 divider = make_axes_locatable(ax)
 cax = divider.append_axes("right", size="3%", pad=0.2)
 cbar = plt.colorbar(im, cax=cax)
@@ -28,7 +21,7 @@ plt.savefig('gaussian1.png', dpi=300)
 
 plt.close()
 fig, ax = plt.subplots()
-im = plt.imshow(gaussian2, cmap='rainbow')
+im = plt.imshow(gaussian2, origin='lower', cmap='rainbow')
 divider = make_axes_locatable(ax)
 cax = divider.append_axes("right", size="3%", pad=0.2)
 cbar = plt.colorbar(im, cax=cax)
